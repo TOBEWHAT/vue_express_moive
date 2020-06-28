98015728a73d3382a61f026686802ef90f1b9810
TOBEWHAT <mr.lihaiting.jp@outlook.com>
2020-06-28 18:23:09 +0800

    04

---
+++ server/config/index.js
@@ -0,0 +1,19 @@
+/* eslint-disable comma-dangle */
+/**
+ * Sequelize : npm i sequelize
+ * 數據庫文件配置
+ */
+const path = require('path')
+
+module.exports = {
+  db: {
+    database: process.env.DATABASE || 'movie',
+    username: 'movie',
+    password: 'movie',
+    options: {
+      host: 'localhost',
+      dialect: 'sqlite',
+      storage: path.resolve(__dirname, '../db/movie.sqlite'), // sqlite需要指定目錄
+    },
+  },
+}
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const { sequelize } = require('./models') // 数据库配置引入
 
 const app = express()
 app.use(bodyParser.json())
@@ -22,4 +23,13 @@ app.post('/users', (req, res) => {
   })
 })
 
-app.listen(3000, () => console.log('Server has been started on port 3000'))
+// 数据库配置执行
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.')
+    app.listen(3000, () => console.log('Server has been started on port 3000'))
+  })
+  .catch((err) => {
+    console.log('Unable to connect to the database:', err)
+  })
